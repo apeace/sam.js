de559b4f531a33a8ecac65e9cbbd92c0d5e91d8e
Andrew Peace <apeace@gmail.com>
2013-10-27 00:57:32 -0400

    just making sure i understand the protocol ;) it works!

--- sam.js
+++ sam.js
@@ -2,11 +2,13 @@ var net = require('net');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
+// beginnings of a SAM client (http://www.i2p2.de/samv3.html)
+// will handle the HELLO handshake and emit commands as events
+// currently logging all data because, well, in-progress
 var SAM = module.exports = function () {
   EventEmitter.call(this);
   this.sock = null;
   this.connectCallback = null;
-  this.handshakeComplete = false;
   this.commandBuffer = '';
 };
 
@@ -69,6 +71,7 @@ proto._onClose = function (had_error) {
 
 SAM.COMMAND_LINE = /^([^\n]+)\n/;
 proto._onData = function (data) {
+  console.log(data);
   if (this.commandBuffer) {
     data = this.commandBuffer + data;
   }
@@ -96,18 +99,20 @@ function parseCommand (cmd) {
   };
 }
 
+// playing around, making sure i understand the protocol
+
 var samSock = new SAM();
 
 samSock.on('handshake', function (success, args) {
   if (!success) throw new Error(args);
   console.log('handshake complete');
-  samSock.sock.write('SESSION CREATE STYLE=STREAM ID=test2 DESTINATION=TRANSIENT\n');
+  samSock.sock.write('SESSION CREATE STYLE=STREAM ID=test3 DESTINATION=TRANSIENT\n');
 });
 
 samSock.on('SESSION STATUS', function (args) {
   if (!args.match(/RESULT=OK/)) throw new Error('Session not created: ' + args);
   console.log('session created');
-  samSock.end();
+  streamingTest();
 });
 
 samSock.on('error', function (err) {
@@ -121,4 +126,26 @@ samSock.on('close', function () {
 });
 
 samSock.connect();
+
+var pastethis_dot_i2p = "Okd5sN9hFWx-sr0HH8EFaxkeIMi6PC5eGTcjM1KB7uQ0ffCUJ2nVKzcsKZFHQc7pLONjOs2LmG5H-2SheVH504EfLZnoB7vxoamhOMENnDABkIRGGoRisc5AcJXQ759LraLRdiGSR0WTHQ0O1TU0hAz7vAv3SOaDp9OwNDr9u902qFzzTKjUTG5vMTayjTkLo2kOwi6NVchDeEj9M7mjj5ySgySbD48QpzBgcqw1R27oIoHQmjgbtbmV2sBL-2Tpyh3lRe1Vip0-K0Sf4D-Zv78MzSh8ibdxNcZACmZiVODpgMj2ejWJHxAEz41RsfBpazPV0d38Mfg4wzaS95R5hBBo6SdAM4h5vcZ5ESRiheLxJbW0vBpLRd4mNvtKOrcEtyCvtvsP3FpA-6IKVswyZpHgr3wn6ndDHiVCiLAQZws4MsIUE1nkfxKpKtAnFZtPrrB8eh7QO9CkH2JBhj7bG0ED6mV5~X5iqi52UpsZ8gnjZTgyG5pOF8RcFrk86kHxAAAA";
+var stats_dot_i2p = "Okd5sN9hFWx-sr0HH8EFaxkeIMi6PC5eGTcjM1KB7uQ0ffCUJ2nVKzcsKZFHQc7pLONjOs2LmG5H-2SheVH504EfLZnoB7vxoamhOMENnDABkIRGGoRisc5AcJXQ759LraLRdiGSR0WTHQ0O1TU0hAz7vAv3SOaDp9OwNDr9u902qFzzTKjUTG5vMTayjTkLo2kOwi6NVchDeEj9M7mjj5ySgySbD48QpzBgcqw1R27oIoHQmjgbtbmV2sBL-2Tpyh3lRe1Vip0-K0Sf4D-Zv78MzSh8ibdxNcZACmZiVODpgMj2ejWJHxAEz41RsfBpazPV0d38Mfg4wzaS95R5hBBo6SdAM4h5vcZ5ESRiheLxJbW0vBpLRd4mNvtKOrcEtyCvtvsP3FpA-6IKVswyZpHgr3wn6ndDHiVCiLAQZws4MsIUE1nkfxKpKtAnFZtPrrB8eh7QO9CkH2JBhj7bG0ED6mV5~X5iqi52UpsZ8gnjZTgyG5pOF8RcFrk86kHxAAAA";
+
+function streamingTest () {
+  var streamSock = new SAM();
+  streamSock.on('handshake', function (success, args) {
+    if (!success) throw new Error(args);
+    console.log('second handshake complete');
+    console.log('attempting to connect to stats.i2p...');
+    streamSock.sock.write('STREAM CONNECT ID=test3 DESTINATION=' + stats_dot_i2p + '\n');
+  });
+  streamSock.on('STREAM STATUS', function (args) {
+    if (!args.match(/RESULT=OK/)) {
+      console.log('retrying...');
+      streamSock.end();
+      return streamingTest();
+    }
+    streamSock.sock.write('GET / HTTP/1.0\r\n\r\n');
+  });
+  streamSock.connect();
+}
 
